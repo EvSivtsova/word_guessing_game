d50b14861d02430bca16f9e119bbdc547f9aabe4
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-08 14:18:01 +0000

    Stop the game once the game is won - boolean

--- app/src/main/java/game/App.java
+++ app/src/main/java/game/App.java
@@ -3,7 +3,9 @@
  */
 package game;
 
-import java.util.Scanner;  
+import java.util.Scanner;
+
+import org.checkerframework.checker.units.qual.A;  
 
 public class App {
     public String getGreeting() {
@@ -15,15 +17,19 @@ public class App {
         Game game = new Game(new WordChoser());
         System.out.printf("Welcome! Today the word to guess is:");
         Scanner scanner = new Scanner(System.in);
-        while (game.remainingAttempts > 0) {
-            System.out.println(game.getWordToGuess(new Masker()));
-            System.out.printf("Enter one letter to guess (%d attemps remaining):\n", game.remainingAttempts);
+        do { System.out.println(game.getWordToGuess(new Masker()));
+            System.out.printf("Enter one letter to guess (%d attempts remaining):\n", game.remainingAttempts);
             Character guessedLetter = scanner.nextLine().charAt(0);
             if (game.guessLetter(guessedLetter)) {
                 System.out.println("Right!");
             } else {
                 System.out.println("Wrong...");
             }
+        } while (game.remainingAttempts > 0 && game.on());
+        if (game.on() == false) {
+            System.out.println("Congratulations! You won!");
+        } else {
+            System.out.println("You lost this time! Try your luck again!");
         }
         scanner.close();
     }

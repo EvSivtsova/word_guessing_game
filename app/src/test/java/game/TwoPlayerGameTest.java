57996958da043c480c447e54ff18c478aa142162
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-15 18:44:19 +0000

    Test for identifyWinner() in TwoPlayerGame - 1

--- app/src/test/java/game/TwoPlayerGameTest.java
+++ app/src/test/java/game/TwoPlayerGameTest.java
@@ -19,5 +19,20 @@ public class TwoPlayerGameTest {
         assertTrue("Returns true if neither of the players won or lost.", game.twoPlayerGameOn(players));
     }
 
+    @Test
+    public void testIdentifiesWinner_WhenTheFistPlayerWon() {
+        Game mockedGame1 = mock(Game.class);
+        when(mockedGame1.gameWon()).thenReturn(true);
+        Game mockedGame2 = mock(Game.class);
+        when(mockedGame2.gameWon()).thenReturn(false);
+        Game[] players =  {mockedGame1, mockedGame2};
+
+        TwoPlayerGame game = new TwoPlayerGame();
+        game.identifyWinner(players);
+        assertEquals("The first player is the winner if they won the game.", 1, game.getWinner());
+        assertEquals("The second player is the loser if the first player won the game.", 2, game.getLoser());
+
+    }
+
 
 }

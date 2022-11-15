0c098327c518fae97fcb98cbc1de6bb33b075ef0
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-15 18:39:15 +0000

    Test for twoPlayerGameOn() in TwoPlayerGame

--- app/src/test/java/game/TwoPlayerGameTest.java
+++ app/src/test/java/game/TwoPlayerGameTest.java
@@ -2,8 +2,22 @@ package game;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class TwoPlayerGameTest {
     @Test
+    public void testGameIsOn_WhenNoPlayerLostOrWon() {
+        Game mockedGame1 = mock(Game.class);
+        when(mockedGame1.gameOver()).thenReturn(false);
+        Game mockedGame2 = mock(Game.class);
+        when(mockedGame2.gameOver()).thenReturn(false);
+        Game[] players =  {mockedGame1, mockedGame2};
+
+        TwoPlayerGame game = new TwoPlayerGame();
+        assertTrue("Returns true if neither of the players won or lost.", game.twoPlayerGameOn(players));
+    }
 
 
 }

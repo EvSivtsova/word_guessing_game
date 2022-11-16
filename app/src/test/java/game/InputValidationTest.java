babd2f0087d5de0b109ca5c170a05dc1bfec3399
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-16 08:25:37 +0000

    Validate letter input - InputValidation 2

--- app/src/test/java/game/InputValidationTest.java
+++ app/src/test/java/game/InputValidationTest.java
@@ -2,6 +2,7 @@ package game;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class InputValidationTest {
@@ -10,5 +11,12 @@ public class InputValidationTest {
         char letter = 'A';
         InputValidation check = new InputValidation();
         assertTrue("Returns true of char is a letter", check.validateLetter(letter));
+    }
+
+    @Test
+    public void testValidatesGuessedLetter_WhenNotALetter() {
+        char letter = '1';
+        InputValidation check = new InputValidation();
+        assertFalse("Returns false of char is not a letter", check.validateLetter(letter));
     }
 }

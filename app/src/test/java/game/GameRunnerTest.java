c15fc740999f4e9b3843389f084d017632adcb97
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-19 15:06:41 +0000

    Test concludeTwoPlayerGame - GameRunner

--- app/src/test/java/game/GameRunnerTest.java
+++ app/src/test/java/game/GameRunnerTest.java
@@ -1,9 +1,6 @@
 package game;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -16,6 +13,7 @@ public class GameRunnerTest {
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
 
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -49,6 +47,22 @@ public class GameRunnerTest {
         GameRunner gameRunner = new GameRunner();
         gameRunner.concludeOnePlayerGame(game);
         Assert.assertEquals(expectedResult, outContent.toString());
+    }
+
+    @Test
+    public void testDisplaysWinnerAndLoserMessages_WhenTwoPlayerGame() {
+        TwoPlayerGame game = mock(TwoPlayerGame.class);
+        when(game.getWinnersName()).thenReturn("Alex");
+        when(game.getLosersName()).thenReturn("Steve");
+        when(game.getLoser()).thenReturn((byte) 2);
+
+        String expectedWinnerMessage = ANSI_BLUE_BACKGROUND + ANSI_BLACK + "         Congratulations, Alex! You won!          " + ANSI_RESET + "\n\n" +
+                ANSI_YELLOW + "Steve, you lost this time! Try your luck again!" + ANSI_RESET + "\n\n";
+
+        GameRunner gameRunner = new GameRunner();
+        gameRunner.concludeTwoPlayerGame(game);
+        Assert.assertEquals(expectedWinnerMessage, outContent.toString());
+        Assert.assertEquals(expectedWinnerMessage, outContent.toString());
     }
 
     @After
